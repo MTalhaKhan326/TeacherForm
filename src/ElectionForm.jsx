f1e32d50dcb1d391e47744007fc04c33faa704ca
Talhakhan326 <muhammadtalhakhan326@gmail.com>
2024-03-06 20:23:19 +0500

    first commit

--- src/ElectionForm.jsx
+++ src/ElectionForm.jsx
@@ -111,16 +111,16 @@ const Electionform = () => {
   const submitNo = async(e)=>{
     setModalMessage(`آپ کا جواب دینا کا بہت شکریہ ہم جلد ہی آپ سے رابطہ کریں گے۔`);
     setShowModal1(true);
-    // const apiUrl = `https://cms-managment.vercel.app/election/api/update-log-report/${cnic}`;
-    //   const requestBody = {
-    //      "payment_received": false,
-    //      "interested_in_more_work": true
-    // };
-    // await axios.put(apiUrl, requestBody).then(res => {
-    //   console.log("Resss",res.data.message)
-    // }).catch(e => {
-    //   console.log(e)
-    // })
+    const apiUrl = `https://cms-managment.vercel.app/election/api/update-log-report/${cnic}`;
+      const requestBody = {
+         "payment_received": false,
+         "interested_in_more_work": true
+    };
+    await axios.put(apiUrl, requestBody).then(res => {
+      console.log("Resss",res.data.message)
+    }).catch(e => {
+      console.log(e)
+    })
   }
    
   const submitListYes = async(e)=>{
@@ -372,7 +372,7 @@ const Electionform = () => {
                       style={{ filter: "brightness(5) invert(1)" }}
                     />
                   </div>
-                  {/* <div>
+                  <div>
                      <img
                     src={AppImages.heart}
                     alt=""
@@ -382,7 +382,7 @@ const Electionform = () => {
                     // style={{ filter: 'brightness(5) invert(1)' }}
                     onClick={handleHeartClick}
                   />
-                  </div> */}
+                  </div>
                   
           </div>
         </div>
@@ -440,7 +440,7 @@ const Electionform = () => {
          </div>}
         </form>
         {
-          response === '' ? (<div> <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mt-[60px]">
+          response === '' ? (<div> <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mt-4">
         <div className=" text-blue-500 p-2 font-bold text-[18px] hover:text-blue-400" onClick={() => handleGovernmentClick('Punjab Government')}>
            Punjab Government
         </div>
@@ -677,8 +677,9 @@ const Electionform = () => {
           },
         }}
       >
-        {
-          modalMessage1 === '' ? ( < form onSubmit={handleSubmit1} className="max-w-md mx-auto px-4" dir='rtl'>
+        {/* {
+          modalMessage1 === '' ? ( 
+          < form onSubmit={handleSubmit1} className="max-w-md mx-auto px-4" dir='rtl'>
           <div className="mb-4">
            <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2">
              نام
@@ -747,12 +748,12 @@ const Electionform = () => {
             {loading ? <Loading /> :  'Submit'}
            </button>
          </div>
-         </form>):(
+         </form>):( */}
           <div className='text-center'>
             <div className='font-semibold text-slate-600 text-[18px]'>{modalMessage1}</div>
           </div>
-         )
-        }
+         {/* ) */}
+        {/* } */}
       
         {/* <div className="text-center"> */}
           {/*  */}
